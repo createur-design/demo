b8b6a0de08afe1be6fad11f78ae2c2ce4bacffc2
Christophe Delobel <cdelobel.ext@simplon.co>
2021-01-25 11:48:14 +0100

    update

--- js/app.js
+++ js/app.js
@@ -1,17 +1,17 @@
-const message =
-  "<p><b>Simplon vous remercie</b> d'avoir participer à notre atelier 😍</p>";
-let countDown = 5;
+import messageSecret from "./message.js";
 
-const body = document.querySelector("body");
 const compteur = document.querySelector("#compteur");
-const divMessage = document.querySelector("#message");
+const message = document.querySelector("#message");
 
-const count = setInterval(() => {
-  compteur.textContent = countDown;
-  countDown -= 1;
-  if (countDown < 0) {
-    clearInterval(count);
-    divMessage.innerHTML = message;
-    body.classList.add("final");
+let count = 5;
+
+const countDown = setInterval(() => {
+  compteur.textContent = count;
+  count -= 1;
+  if (count < 0) {
+    clearInterval(countDown);
+    message.innerHTML = messageSecret;
+    compteur.style.display = "none";
+    message.style.display = "block";
   }
 }, 1000);
---
+++ js/message.js
@@ -0,0 +1,4 @@
+const message =
+  "<p><b>Simplon vous remercie</b> d'avoir participer à notre atelier 😍</p>";
+
+export default message;
